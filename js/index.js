51590d85379752a2c84b62af43acf85224159ef4
Menno Finlay-Smits <hello@menno.io>
2023-04-06 23:44:51 +1200

    Ported webworld to rustpack template

--- js/index.js
+++ js/index.js
@@ -1,1 +1,7 @@
-import("../pkg/index.js").catch(console.error);
+import('../pkg')
+    .then(wasm => {
+        const canvas = document.getElementById('drawing');
+        const ctx = canvas.getContext('2d');
+        wasm.draw(ctx, 1000, 1000, window.performance.now());
+    })
+    .catch(console.error);
--- src/lib.rs
+++ src/lib.rs
@@ -1,24 +1,69 @@
+use noise::{utils::*, Clamp, Fbm, Seedable};
 use wasm_bindgen::prelude::*;
-use web_sys::console;
+use wasm_bindgen::Clamped;
+use web_sys::{CanvasRenderingContext2d, ImageData};
 
-// When the `wee_alloc` feature is enabled, this uses `wee_alloc` as the global
-// allocator.
-//
-// If you don't want to use `wee_alloc`, you can safely delete this.
-#[cfg(feature = "wee_alloc")]
-#[global_allocator]
-static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+const SEA_LEVEL: f64 = -0.04;
+const PERSISTENCE: f64 = 0.4;
 
-// This is like the `main` function, except for JavaScript.
-#[wasm_bindgen(start)]
-pub fn main_js() -> Result<(), JsValue> {
-    // This provides better error messages in debug mode.
-    // It's disabled in release mode so it doesn't bloat up the file size.
-    #[cfg(debug_assertions)]
-    console_error_panic_hook::set_once();
+#[wasm_bindgen]
+pub fn draw(
+    ctx: &CanvasRenderingContext2d,
+    width: u32,
+    height: u32,
+    seed: u32,
+) -> Result<(), JsValue> {
+    let mut data = generate_map(width, height, seed);
+    let data = ImageData::new_with_u8_clamped_array_and_sh(Clamped(&mut data), width, height)?;
+    ctx.put_image_data(&data, 0.0, 0.0)
+}
 
-    // Your code goes here!
-    console::log_1(&JsValue::from_str("Hello, world?"));
+fn generate_map(width: u32, height: u32, seed: u32) -> Vec<u8> {
+    let width: usize = width.try_into().unwrap();
+    let height: usize = height.try_into().unwrap();
+    let out_scale = width as f64 / height as f64;
 
-    Ok(())
+    let mut land_base = Fbm::new().set_seed(seed);
+    land_base.persistence = PERSISTENCE;
+
+    // Avoid shadows on seas by flattening these to sea leel
+    let sea_clamped = Clamp::new(&land_base).set_lower_bound(SEA_LEVEL);
+
+    // XXX scaling is wrong as soon as it's not square
+    let map = PlaneMapBuilder::new(&sea_clamped)
+        .set_size(width, height)
+        .set_x_bounds(-out_scale, out_scale)
+        .set_y_bounds(-out_scale, out_scale)
+        .build();
+
+    let grad = ColorGradient::new()
+        .clear_gradient()
+        .add_gradient_point(-1.00, [0, 0, 160, 255]) // deep sea
+        .add_gradient_point(SEA_LEVEL, [0, 0, 164, 255]) // shallow sea
+        .add_gradient_point(-0.02, [192, 192, 128, 255]) // beach
+        .add_gradient_point(0.00, [0, 128, 0, 255]) //low forest
+        .add_gradient_point(0.15, [0, 192, 0, 255]) //high forest
+        .add_gradient_point(0.18, [172, 172, 172, 255]) //rock
+        .add_gradient_point(0.28, [192, 192, 192, 255]) // high rock
+        .add_gradient_point(0.29, [252, 252, 252, 255]) //snow
+        .add_gradient_point(1.00, [255, 255, 255, 255]);
+
+    let mut renderer = ImageRenderer::new();
+    renderer.enable_light();
+    let image = renderer
+        .set_gradient(grad)
+        .set_light_elevation(30.0)
+        .set_light_azimuth(270.0)
+        .set_light_contrast(20.0)
+        .set_light_brightness(2.0)
+        .render(&map);
+
+    let mut data = Vec::new();
+    for x in 0..width {
+        for y in 0..height {
+            let c = image.get_value(x, y);
+            data.extend(c);
+        }
+    }
+    data
 }
