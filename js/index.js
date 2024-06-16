a48e21ad6ceededa2d97ee95b31ffcaafa09bd95
Menno Finlay-Smits <hello@menno.io>
2024-06-16 23:33:44 +1200

    Rescale and redraw when browser window size changes
    ... and reseed when map is clicked.

--- js/index.js
+++ js/index.js
@@ -1,12 +1,21 @@
 import('../pkg')
     .then(wasm => {
-        const dim = Math.max(window.innerWidth, window.innerHeight);
-
+        let seed = window.performance.now();
         const canvas = document.getElementById('drawing');
-        canvas.width = dim;
-        canvas.height = dim;
-
         const ctx = canvas.getContext('2d');
-        wasm.draw(ctx, dim, dim, window.performance.now());
+
+        const redraw = () => {
+            const dim = Math.max(window.innerWidth, window.innerHeight);
+            canvas.width = dim;
+            canvas.height = dim;
+            wasm.draw(ctx, dim, dim, seed);
+        };
+        redraw();
+
+        window.onresize = redraw;
+        canvas.onclick = () => {
+            seed = window.performance.now();
+            redraw();
+        };
     })
     .catch(console.error);
