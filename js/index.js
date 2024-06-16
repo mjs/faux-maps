4697dd4f9396f072a8ec86a81732bb4acdb3779b
Menno Finlay-Smits <hello@menno.io>
2024-06-16 23:18:16 +1200

    Size output to browser window

--- js/index.js
+++ js/index.js
@@ -1,7 +1,12 @@
 import('../pkg')
     .then(wasm => {
+        const dim = Math.max(window.innerWidth, window.innerHeight);
+
         const canvas = document.getElementById('drawing');
+        canvas.width = dim;
+        canvas.height = dim;
+
         const ctx = canvas.getContext('2d');
-        wasm.draw(ctx, 1000, 1000, window.performance.now());
+        wasm.draw(ctx, dim, dim, window.performance.now());
     })
     .catch(console.error);
